c155ceef2f0f0420bb2ef53d8519711162ca19fd
Mazyar Sadat-Razavi <msadatra@cisco.com>
2020-01-27 15:53:36 -0800

    Adding support for firmwareOSource MO

---
+++ client/firmwareOSource_service.go
@@ -0,0 +1,48 @@
+package client
+  
+import (
+        "fmt"
+        "github.com/ciscoecosystem/aci-go-client/models"
+)
+
+func (sm *ServiceManager) CreateOSource(name string, description string, firmwareOSourceAttr models.OSourceAttributes) (*models.OSource, error) {
+        rn := fmt.Sprintf("osrc-%s",name)
+        parentDn := fmt.Sprintf("uni/fabric/fwrepop")
+        firmwareOSource := models.NewOSource(rn, parentDn, description, firmwareOSourceAttr)
+        err := sm.Save(firmwareOSource)
+        return firmwareOSource, err
+}
+
+func (sm *ServiceManager) ReadOSource(name string) (*models.OSource, error) {
+        dn := fmt.Sprintf("uni/fabric/fwrepop/osrc-%s", name )
+        cont, err := sm.Get(dn)
+        if err != nil {
+                return nil, err
+        }
+        firmwareOSource := models.OSourceFromContainer(cont)
+        return firmwareOSource, nil
+}
+
+func (sm *ServiceManager) DeleteOSource(name string ) error {
+        dn := fmt.Sprintf("uni/fabric/fwrepop/osrc-%s", name)
+        return sm.DeleteByDn(dn, models.FirmwareOSourceName)
+}
+
+func (sm *ServiceManager) UpdateOSource(name string, description string, firmwareOSourceAttr models.OSourceAttributes) (*models.OSource, error) {
+        rn := fmt.Sprintf("osrc-%s",name)
+        parentDn := fmt.Sprintf("uni/fabric/fwrepop")
+        firmwareOSource := models.NewOSource(rn, parentDn, description, firmwareOSourceAttr)
+        firmwareOSource.Status = "modified"
+        err := sm.Save(firmwareOSource)
+        return firmwareOSource, err
+
+}
+
+func (sm *ServiceManager) ListOSource() ([]*models.OSource, error) {
+        baseurlStr := "/api/node/class"
+        dnUrl := fmt.Sprintf("%s/firmwareOSource.json", baseurlStr )
+        cont, err := sm.GetViaURL(dnUrl)
+        list := models.OSourceListFromContainer(cont)
+        return list, err
+}
+
---
+++ models/firmware_osource.go
@@ -0,0 +1,100 @@
+package models
+
+
+import (
+        "fmt"
+        "strconv"
+
+        "github.com/ciscoecosystem/aci-go-client/container"
+)
+
+const FirmwareOSourceName = "firmwareOSource"
+
+type OSource struct {
+        BaseAttributes
+    OSourceAttributes
+}
+
+type OSourceAttributes struct {
+    Name       string `json:",omitempty"`
+    Url       string `json:",omitempty"`
+    Proto       string `json:",omitempty"`
+    User       string `json:",omitempty"`
+    AuthType       string `json:",omitempty"`
+    AuthPass       string `json:",omitempty"`
+}
+
+func NewOSource(firmwareOSourceRn, parentDn, description string, firmwareOSourceAttr OSourceAttributes) *OSource {
+        dn := fmt.Sprintf("%s/%s", parentDn, firmwareOSourceRn)
+        return &OSource{
+                BaseAttributes: BaseAttributes{
+                        DistinguishedName: dn,
+                        Description:       description,
+                        Status:            "created, modified",
+                        ClassName:         FirmwareOSourceName,
+                        Rn:                firmwareOSourceRn,
+                },
+                OSourceAttributes: firmwareOSourceAttr,
+        }
+}
+
+func (firmwareOSource *OSource) ToMap() (map[string]string, error) {
+        firmwareOSourceMap, err := firmwareOSource.BaseAttributes.ToMap()
+        if err != nil {
+                return nil, err
+        }
+
+    A(firmwareOSourceMap, "name", firmwareOSource.Name)
+    A(firmwareOSourceMap, "urle", firmwareOSource.Url)
+    A(firmwareOSourceMap, "proto", firmwareOSource.Proto)
+    A(firmwareOSourceMap, "user", firmwareOSource.User)
+    A(firmwareOSourceMap, "authType", firmwareOSource.AuthType)
+    A(firmwareOSourceMap, "authPass", firmwareOSource.AuthPass)
+
+    return firmwareOSourceMap, err
+}
+
+func OSourceFromContainerList(cont *container.Container, index int) *OSource {
+
+        OSourceCont := cont.S("imdata").Index(index).S(FirmwareOSourceName, "attributes")
+        return &OSource{
+                BaseAttributes{
+                        DistinguishedName: G(OSourceCont, "dn"),
+                        Description:       G(OSourceCont, "descr"),
+                        Status:            G(OSourceCont, "status"),
+                        ClassName:         FvtenantClassName,
+                        Rn:                G(OSourceCont, "rn"),
+                },
+
+                OSourceAttributes{
+        Name : G(OSourceCont, "name"),
+        Url : G(OSourceCont, "url"),
+
+        Proto : G(OSourceCont, "proto"),
+        User : G(OSourceCont, "user"),
+        AuthType : G(OSourceCont, "authType"),
+        AuthPass : G(OSourceCont, "AuthPass"),
+        },
+
+        }
+}
+
+
+func OSourceFromContainer(cont *container.Container) *OSource {
+
+        return OSourceFromContainerList(cont, 0)
+}
+
+func OSourceListFromContainer(cont *container.Container) []*OSource {
+        length, _ := strconv.Atoi(G(cont, "totalCount"))
+
+        arr := make([]*OSource, length)
+
+        for i := 0; i < length; i++ {
+
+                arr[i] = OSourceFromContainerList(cont, i)
+        }
+
+        return arr
+}
+
