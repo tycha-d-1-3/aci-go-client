2e780958719abc361f0c8d6f1a410644a01920b5
Ty Chau <tycha@cisco.com>
2020-05-20 21:05:40 -0700

    Add CreateCtrlrMaintP for Triggering Controller Policy

--- client/maintMaintGrp_service.go
+++ client/maintMaintGrp_service.go
@@ -1,50 +1,50 @@
 package client
 
 import (
-        "fmt"
-        "github.com/ciscoecosystem/aci-go-client/models"
-	    "github.com/ciscoecosystem/aci-go-client/container"
+	"fmt"
+	"github.com/ciscoecosystem/aci-go-client/container"
+	"github.com/ciscoecosystem/aci-go-client/models"
 )
 
 func (sm *ServiceManager) CreateMaintGrp(name string, description string, maintMaintGrpAttr models.MaintGrpAttributes) (*models.MaintGrp, error) {
-        rn := fmt.Sprintf("maintgrp-%s",name)
-        parentDn := fmt.Sprintf("uni/fabric")
-        maintMaintGrp := models.NewMaintGrp(rn, parentDn, description, maintMaintGrpAttr)
-        err := sm.Save(maintMaintGrp)
-        return maintMaintGrp, err
+	rn := fmt.Sprintf("maintgrp-%s", name)
+	parentDn := fmt.Sprintf("uni/fabric")
+	maintMaintGrp := models.NewMaintGrp(rn, parentDn, description, maintMaintGrpAttr)
+	err := sm.Save(maintMaintGrp)
+	return maintMaintGrp, err
 }
 
 func (sm *ServiceManager) ReadMaintGrp(name string) (*models.MaintGrp, error) {
-        dn := fmt.Sprintf("uni/fabric/maintgrp-%s", name )
-        cont, err := sm.Get(dn)
-        if err != nil {
-                return nil, err
-        }
-        maintMaintGrp := models.MaintGrpFromContainer(cont)
-        return maintMaintGrp, nil
+	dn := fmt.Sprintf("uni/fabric/maintgrp-%s", name)
+	cont, err := sm.Get(dn)
+	if err != nil {
+		return nil, err
+	}
+	maintMaintGrp := models.MaintGrpFromContainer(cont)
+	return maintMaintGrp, nil
 }
 
-func (sm *ServiceManager) DeleteMaintGrp(name string ) error {
-        dn := fmt.Sprintf("uni/fabric/maintgrp-%s", name)
-        return sm.DeleteByDn(dn, models.MaintMaintGrpClassName)
+func (sm *ServiceManager) DeleteMaintGrp(name string) error {
+	dn := fmt.Sprintf("uni/fabric/maintgrp-%s", name)
+	return sm.DeleteByDn(dn, models.MaintMaintGrpClassName)
 }
 
 func (sm *ServiceManager) UpdateMaintGrp(name string, description string, maintMaintGrpAttr models.MaintGrpAttributes) (*models.MaintGrp, error) {
-        rn := fmt.Sprintf("maintgrp-%s",name)
-        parentDn := fmt.Sprintf("uni/fabric")
-        maintMaintGrp := models.NewMaintGrp(rn, parentDn, description, maintMaintGrpAttr)
-        maintMaintGrp.Status = "modified"
-        err := sm.Save(maintMaintGrp)
-        return maintMaintGrp, err
+	rn := fmt.Sprintf("maintgrp-%s", name)
+	parentDn := fmt.Sprintf("uni/fabric")
+	maintMaintGrp := models.NewMaintGrp(rn, parentDn, description, maintMaintGrpAttr)
+	maintMaintGrp.Status = "modified"
+	err := sm.Save(maintMaintGrp)
+	return maintMaintGrp, err
 
 }
 
 func (sm *ServiceManager) ListMaintGrp() ([]*models.MaintGrp, error) {
-        baseurlStr := "/api/node/class"
-        dnUrl := fmt.Sprintf("%s/maintMaintGrp.json", baseurlStr )
-        cont, err := sm.GetViaURL(dnUrl)
-        list := models.MaintGrpListFromContainer(cont)
-        return list, err
+	baseurlStr := "/api/node/class"
+	dnUrl := fmt.Sprintf("%s/maintMaintGrp.json", baseurlStr)
+	cont, err := sm.GetViaURL(dnUrl)
+	list := models.MaintGrpListFromContainer(cont)
+	return list, err
 }
 
 func (sm *ServiceManager) CreateRelationvzRsMgrppFromMaintGrp(parentDn, tnMaintMaintPName string) error {
@@ -76,5 +76,3 @@ func (sm *ServiceManager) CreateRelationvzRsMgrppFromMaintGrp(parentDn, tnMaintM
 
 	return nil
 }
-
-
